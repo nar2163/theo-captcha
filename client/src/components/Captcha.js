ce31a5c1d5d13040c5da4ec37590a2b4d73c66c2
Nicolas Rockwood <nar2163@columbia.edu>
2023-02-19 08:53:14 -0600

    add react useMouse hook

--- client/src/components/Captcha.js
+++ client/src/components/Captcha.js
@@ -1,8 +1,31 @@
+import * as React from 'react'
+import useMouse from '@react-hook/mouse-position'
 import { Box, Checkbox, FormControlLabel, FormGroup } from '@mui/material'
 
 function Captcha({ type = 'checkbox' }) {
+  const [hoverTimeElapsed, setHoverTimeElapsed] = React.useState(0)
+  const ref = React.useRef(null)
+  const hoveringMouse = useMouse(ref, { isOver: false })
+
+  React.useEffect(() => {
+    if (hoveringMouse.isOver) {
+      const timer = setInterval(() => {
+        setHoverTimeElapsed((prevTime) => prevTime + 1)
+      }, 1000)
+
+      return () => {
+        clearInterval(timer)
+      }
+    }
+  }, [hoveringMouse])
+
+  // NOTE: if the user's mouse is in the target Box for less than 3
+  // seconds the secondary captcha triggers
+  console.log(hoverTimeElapsed)
+
   return (
     <Box
+      ref={ref}
       sx={{
         background: '#D3D3D3',
         padding: '7px 15px',
@@ -20,7 +43,7 @@ function Captcha({ type = 'checkbox' }) {
               sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }}
             />
           }
-          label={'I am not a robot'}
+          label={`I am not a robot`}
           sx={{ justifyContent: 'center' }}
         />
       </FormGroup>
